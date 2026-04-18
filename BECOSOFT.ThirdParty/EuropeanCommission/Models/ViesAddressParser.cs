597a397a03b9b02360147e0a3b71bebf72dfff83
Mohammed Isbulla <isbulla@jrcsoftware.com>
2026-04-18 14:42:34 +0400

    Improve address parsing logic for street, number, and city

--- BECOSOFT.ThirdParty/EuropeanCommission/Models/ViesAddressParser.cs
+++ BECOSOFT.ThirdParty/EuropeanCommission/Models/ViesAddressParser.cs
@@ -13,6 +13,43 @@ namespace BECOSOFT.ThirdParty.EuropeanCommission.Models {
             if (response.CountryCode.EqualsIgnoreCase("BE")) {
                 splitCharacters.Add('\u00FF');
             }
+            var addressLines = address
+                    .Split(new[] { "\n"}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+            if (addressLines.Length < 2)
+            {
+                return response;
+            }
+
+            var streetAndNumberLine = addressLines[0];
+            var lastSpaceIndex = streetAndNumberLine.LastIndexOf(' ');
+            if (lastSpaceIndex > 0)
+            {
+                response.Street = streetAndNumberLine.Substring(0, lastSpaceIndex).Trim();
+                response.Number = streetAndNumberLine.Substring(lastSpaceIndex + 1).Trim();
+            }
+            else
+            {
+                response.Street = streetAndNumberLine;
+                response.Number = string.Empty;
+            }
+
+            response.Box = string.Empty;
+            var postalAndCityLine = addressLines[1];
+
+            var firstSpaceIndex = postalAndCityLine.IndexOf(' ');
+            if (firstSpaceIndex > 0)
+            {
+                response.PostalCode = postalAndCityLine.Substring(0, firstSpaceIndex).Trim();
+                response.Place = postalAndCityLine.Substring(firstSpaceIndex + 1).Trim();
+            }
+            else
+            {
+                response.PostalCode = postalAndCityLine;
+                response.Place = string.Empty;
+            }
 
             return response;
         }
